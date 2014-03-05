3f389fd709a7cc73d72bcb84d0adc2d09a58257c
Paulus Schoutsen <Paulus@PaulusSchoutsen.nl>
2014-03-04 23:21:31 -0800

    Version bump because of added UPNP support.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='PyChromecast',
-    version='0.1',
+    version='0.2',
     license='MIT',
     url='https://github.com/balloob/pychromecast',
     author='Paulus Schoutsen',
