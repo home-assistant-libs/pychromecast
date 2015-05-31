fae31237a68591ebf5bf850130e67ba242e777d1
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-05-31 14:53:06 -0700

    Version bump to 0.6.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyChromecast',
-    version='0.6.2',
+    version='0.6.3',
     license='MIT',
     url='https://github.com/balloob/pychromecast',
     author='Paulus Schoutsen',
