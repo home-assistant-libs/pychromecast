d163b12c5224be43ae6f270904c0cf6972cb9758
Jan Borsodi <jborsodi@gmail.com>
2015-10-26 00:18:03 +0100

    Added return statement to BaseController.send_message

--- pychromecast/controllers/__init__.py
+++ pychromecast/controllers/__init__.py
@@ -80,7 +80,7 @@ class BaseController(object):
                     ("Namespace {} is not supported by running"
                      "application.").format(self.namespace))
 
-        self._message_func(
+        return self._message_func(
             self.namespace, data, inc_session_id, wait_for_response)
 
     # pylint: disable=unused-argument,no-self-use
