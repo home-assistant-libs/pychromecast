511c952e007bee042e42f14e23c77a76fc8c013e
Jan Borsodi <jborsodi@gmail.com>
2016-01-23 17:36:41 +0100

    Placed build log in build folder

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,4 @@
+import os
 from fabric.decorators import task
 from fabric.operations import local
 
@@ -7,7 +8,19 @@ def build():
     """
     Builds the distribution files
     """
-    local("date >> build.log")
+    if not os.path.exists("build"):
+        os.mkdir("build")
+    local("date >> build/log")
     local("pandoc README.md -f markdown -t rst -s -o README.rst")
-    local("python setup.py sdist >> build.log")
-    local("python setup.py bdist_wheel >> build.log")
+    local("python setup.py sdist >> build/log")
+    local("python setup.py bdist_wheel >> build/log")
+
+
+@task
+def release():
+    """
+    Uploads files to PyPi to create a new release.
+
+    Note: Requires that files have been built first
+    """
+    local("twine upload dist/*")
