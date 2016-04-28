97aa41a4c573765a9d642fcca9e991b62a4222ba
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-04-28 14:17:59 +0200

    Version bump to 0.7.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open('README.rst').read()
 
 setup(
     name='PyChromecast',
-    version='0.7.2',
+    version='0.7.3',
     license='MIT',
     url='https://github.com/balloob/pychromecast',
     author='Paulus Schoutsen',
