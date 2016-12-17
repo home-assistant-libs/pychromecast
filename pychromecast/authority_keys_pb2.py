414991628dac2c05b422c6bb204bff7e11ecd516
theychx <theychx@users.noreply.github.com>
2016-12-17 03:24:23 +0100

    Update protobuf requirement (#143)
    * Update protobuf requirement
    
    * Update protobuf requirement (3.0.0)
    
    * Update proto and _pb2.py files

--- pychromecast/authority_keys_pb2.py
+++ pychromecast/authority_keys_pb2.py
@@ -17,9 +17,9 @@ _sym_db = _symbol_database.Default()
 
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='authority_keys.proto',
-  package='extensions.core_api.cast_channel.proto',
+  package='extensions.api.cast_channel.proto',
   syntax='proto2',
-  serialized_pb=_b('\n\x14\x61uthority_keys.proto\x12&extensions.core_api.cast_channel.proto\"\x88\x01\n\rAuthorityKeys\x12G\n\x04keys\x18\x01 \x03(\x0b\x32\x39.extensions.core_api.cast_channel.proto.AuthorityKeys.Key\x1a.\n\x03Key\x12\x13\n\x0b\x66ingerprint\x18\x01 \x02(\x0c\x12\x12\n\npublic_key\x18\x02 \x02(\x0c\x42\x02H\x03')
+  serialized_pb=_b('\n\x14\x61uthority_keys.proto\x12!extensions.api.cast_channel.proto\"\x83\x01\n\rAuthorityKeys\x12\x42\n\x04keys\x18\x01 \x03(\x0b\x32\x34.extensions.api.cast_channel.proto.AuthorityKeys.Key\x1a.\n\x03Key\x12\x13\n\x0b\x66ingerprint\x18\x01 \x02(\x0c\x12\x12\n\npublic_key\x18\x02 \x02(\x0c\x42\x02H\x03')
 )
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -28,20 +28,20 @@ _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 _AUTHORITYKEYS_KEY = _descriptor.Descriptor(
   name='Key',
-  full_name='extensions.core_api.cast_channel.proto.AuthorityKeys.Key',
+  full_name='extensions.api.cast_channel.proto.AuthorityKeys.Key',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='fingerprint', full_name='extensions.core_api.cast_channel.proto.AuthorityKeys.Key.fingerprint', index=0,
+      name='fingerprint', full_name='extensions.api.cast_channel.proto.AuthorityKeys.Key.fingerprint', index=0,
       number=1, type=12, cpp_type=9, label=2,
       has_default_value=False, default_value=_b(""),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='public_key', full_name='extensions.core_api.cast_channel.proto.AuthorityKeys.Key.public_key', index=1,
+      name='public_key', full_name='extensions.api.cast_channel.proto.AuthorityKeys.Key.public_key', index=1,
       number=2, type=12, cpp_type=9, label=2,
       has_default_value=False, default_value=_b(""),
       message_type=None, enum_type=None, containing_type=None,
@@ -59,19 +59,19 @@ _AUTHORITYKEYS_KEY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=155,
-  serialized_end=201,
+  serialized_start=145,
+  serialized_end=191,
 )
 
 _AUTHORITYKEYS = _descriptor.Descriptor(
   name='AuthorityKeys',
-  full_name='extensions.core_api.cast_channel.proto.AuthorityKeys',
+  full_name='extensions.api.cast_channel.proto.AuthorityKeys',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='keys', full_name='extensions.core_api.cast_channel.proto.AuthorityKeys.keys', index=0,
+      name='keys', full_name='extensions.api.cast_channel.proto.AuthorityKeys.keys', index=0,
       number=1, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
@@ -89,8 +89,8 @@ _AUTHORITYKEYS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=65,
-  serialized_end=201,
+  serialized_start=60,
+  serialized_end=191,
 )
 
 _AUTHORITYKEYS_KEY.containing_type = _AUTHORITYKEYS
@@ -102,12 +102,12 @@ AuthorityKeys = _reflection.GeneratedProtocolMessageType('AuthorityKeys', (_mess
   Key = _reflection.GeneratedProtocolMessageType('Key', (_message.Message,), dict(
     DESCRIPTOR = _AUTHORITYKEYS_KEY,
     __module__ = 'authority_keys_pb2'
-    # @@protoc_insertion_point(class_scope:extensions.core_api.cast_channel.proto.AuthorityKeys.Key)
+    # @@protoc_insertion_point(class_scope:extensions.api.cast_channel.proto.AuthorityKeys.Key)
     ))
   ,
   DESCRIPTOR = _AUTHORITYKEYS,
   __module__ = 'authority_keys_pb2'
-  # @@protoc_insertion_point(class_scope:extensions.core_api.cast_channel.proto.AuthorityKeys)
+  # @@protoc_insertion_point(class_scope:extensions.api.cast_channel.proto.AuthorityKeys)
   ))
 _sym_db.RegisterMessage(AuthorityKeys)
 _sym_db.RegisterMessage(AuthorityKeys.Key)
