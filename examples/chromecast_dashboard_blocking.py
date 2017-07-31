f9d663e04271205e2b0cf74f26e1978e0f979eaf
John Wells <madmodmail@gmail.com>
2017-07-31 16:31:23 -0600

    Add example for chromecastDashboard controller.

---
+++ examples/chromecast_dashboard_blocking.py
@@ -0,0 +1,46 @@
+"""
+Example that shows how the new Python 2 socket client can be used.
+
+Functions called in this example are blocking which means that
+the function doesn't return as long as no result was received.
+"""
+
+from __future__ import print_function
+import time
+import sys
+import logging
+
+import pychromecast
+import pychromecast.controllers.chromecastDashboard as dashboard
+
+if '--show-debug' in sys.argv:
+    logging.basicConfig(level=logging.DEBUG)
+
+casts = pychromecast.get_chromecasts()
+if len(casts) == 0:
+    print("No Devices Found")
+    exit()
+cast = next(cc for cc in casts if cc.device.friendly_name == "Bedroom Display")
+#cast = casts[0]
+
+d = dashboard.ChromecastDashboardController()
+cast.register_handler(d)
+
+print()
+print(cast.device)
+time.sleep(1)
+print()
+print(cast.status)
+print()
+print(cast.media_controller.status)
+print()
+
+if not cast.is_idle:
+    print("Killing current running app")
+    cast.quit_app()
+    time.sleep(5)
+
+time.sleep(1)
+
+d.load_url('http://hass.io')
+
