ec5ef146b3d927c3cb68d0809022fbca03f5057b
Enrique Gonzalez <egonzalezh94@gmail.com>
2018-04-08 14:16:01 -0700

    Remove external dependencies
    The example will now have how to get the access token yourself and how to use spotipy to avoid adding dependencies to pychromecast

--- examples/spotify_example.py
+++ examples/spotify_example.py
@@ -1,14 +1,38 @@
+"""
+Example on how to use the Spotify Controller.
+NOTE: You need to install the spotipy and spotify-token dependencies.
+
+This can be done by running the following:
+pip install spotify-token
+pip install git+https://github.com/plamere/spotipy.git
+"""
 import pychromecast
 from pychromecast.controllers.spotify import SpotifyController
+import spotify_token as st
+import spotipy
 
 chromecasts = pychromecast.get_chromecasts()
 cast = chromecasts[0]
 
 CAST_NAME = "My Chromecast"
+device_id = None
 
 if cast.name == CAST_NAME:
-    sp = SpotifyController(CAST_NAME,"SPOTIFY_USERNAME","SPOTIFY_PASSWORD")
+
+    data = st.start_session("SPOTIFY_USERNAME", "SPOTIFY_PASSWORD")
+    access_token = data[0]
+
+    client = spotipy.Spotify(auth=access_token)
+
+    sp = SpotifyController(CAST_NAME, access_token)
     cast.register_handler(sp)
     sp.launch_app()
 
-    sp.play_song("spotify:track:3Zwu2K0Qa5sT6teCCHPShP")
+    devices_available = client.devices()
+
+    for device in devices_available['devices']:
+        if device['name'] == CAST_NAME and device['type'] == 'CastVideo':
+            device_id = device['id']
+            break
+
+    client.start_playback(device_id, "spotify:track:3Zwu2K0Qa5sT6teCCHPShP")
--- pychromecast/controllers/spotify.py
+++ pychromecast/controllers/spotify.py
@@ -3,8 +3,6 @@ Controller to interface with the DashCast app namespace.
 """
 import logging
 import time
-import spotipy
-import spotify_token
 
 from . import BaseController
 from ..config import APP_SPOTIFY
@@ -23,18 +21,12 @@ class SpotifyController(BaseController):
     # pylint: disable=useless-super-delegation
     # The pylint rule useless-super-delegation doesn't realize
     # we are setting default values here.
-    def __init__(self, friendly_name, username=None, password=None):
+    def __init__(self, access_token):
         super(SpotifyController, self).__init__(APP_NAMESPACE, APP_SPOTIFY)
 
         self.logger = logging.getLogger(__name__)
-        self.username = username
-        self.password = password
-        self.cast_name = friendly_name
         self.session_started = False
-        self.token = None
-        self.expiration_date = None
-        self.client = None
-        self.device_id = None
+        self.access_token = access_token
         self.is_launched = False
     # pylint: enable=useless-super-delegation
 
@@ -50,63 +42,10 @@ class SpotifyController(BaseController):
 
         def callback():
             """Callback function"""
-            self.send_message({"type": TYPE_STATUS, "credentials": self.token})
+            self.send_message({"type": TYPE_STATUS, "credentials": self.access_token})
 
-        curr_time = time.time()
-
-        if self.session_started and curr_time < self.expiration_date:
-            self.logger.debug("Using same token: %s", self.token)
-            self.launch(callback_function=callback)
-        else:
-            self.logger.debug("Creating new token")
-
-            data = spotify_token.start_session(self.username, self.password)
-            self.session_started = True
-            self.token = data[0]
-            self.expiration_date = data[1]
-
-            self.logger.debug("Token is: %s", self.token)
-            self.launch(callback_function=callback)
+        self.launch(callback_function=callback)
 
         # Need to wait for Spotify to be launched on Chromecast completely
         while not self.is_launched:
             time.sleep(1)
-
-        self.client = spotipy.Spotify(auth=self.token)
-
-        self.device_id = self.get_spotify_device_id()
-
-    def get_spotify_device_id(self):
-        """
-        Gets device id from Spotify. This should be the same as the name of
-        the Chromecast.
-        """
-        devices_available = self.client.devices()
-
-        for device in devices_available['devices']:
-            self.logger.debug(device)
-            if (device['name'] == self.cast_name and
-                    device['type'] == 'CastVideo'):
-                return device['id']
-        return None
-
-    def play_song(self, uri):
-        """ Play a single song with it's Spotify URI. """
-        if self.device_id is None:
-            raise Exception("No device id. Try launching app again")
-        self.client.start_playback(device_id=self.device_id, uris=[uri])
-
-    def play_songs(self, uris):
-        """ Play several songs with a list of uris. """
-        if self.device_id is None:
-            raise Exception("No device id. Try launching app again")
-        self.client.start_playback(device_id=self.device_id, uris=uris)
-
-    def play_context(self, context_uri, offset=None):
-        """ Play a Spotify context.
-            Valid contexts are albums, artists and playlists.
-        """
-        if self.device_id is None:
-            raise Exception("No device id. Try launching app again")
-        self.client.start_playback(device_id=self.device_id,
-                                   context_uri=context_uri, offset=offset)
