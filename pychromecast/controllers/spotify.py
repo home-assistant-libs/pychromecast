f80b4a79da9041cc11d1a8042e6932383e467114
Enrique Gonzalez <egonzalezh94@gmail.com>
2018-04-07 21:51:16 -0700

    Remove access token logic from Spotify Controller code
    This was moved to it's own repo: https://github.com/enriquegh/spotify-webplayer-token

--- pychromecast/controllers/spotify.py
+++ pychromecast/controllers/spotify.py
@@ -2,10 +2,9 @@
 Controller to interface with the DashCast app namespace.
 """
 import logging
-import os
 import time
-import requests
 import spotipy
+import spotify_token
 
 from pychromecast.controllers import BaseController
 from ..config import APP_SPOTIFY
@@ -13,9 +12,6 @@ from ..config import APP_SPOTIFY
 logging.basicConfig(level=logging.DEBUG)
 
 APP_NAMESPACE = "urn:x-cast:com.spotify.chromecast.secure.v1"
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) \
-AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
-
 TYPE_STATUS = "setCredentials"
 
 # pylint: disable=too-many-instance-attributes
@@ -44,68 +40,7 @@ class SpotifyController(BaseController):
         """ Currently not doing anything with received messages. """
         return True
 
-    def _get_csrf(self, session, cookies):
-        """ Get CSRF token for Spotify login. """
-        headers = {'user-agent': USER_AGENT}
 
-        response = session.get("https://accounts.spotify.com/login",
-                               headers=headers, cookies=cookies)
-        response.raise_for_status()
-
-        return response.cookies['csrf_token']
-
-    # pylint: disable=too-many-arguments
-    def _login(self, session, cookies, username, password, csrf_token):
-        """ Logs in with CSRF token and cookie within session. """
-        headers = {'user-agent': USER_AGENT}
-
-        data = {"remember": False, "username": username, "password": password,
-                "csrf_token": csrf_token}
-
-        response = session.post("https://accounts.spotify.com/api/login",
-                                data=data, cookies=cookies, headers=headers)
-
-        response.raise_for_status()
-
-    def _get_access_token(self, session, cookies):
-        """ Gets access token after login has been successful. """
-        headers = {'user-agent': USER_AGENT}
-
-        response = session.get("https://open.spotify.com/browse",
-                               headers=headers, cookies=cookies)
-        response.raise_for_status()
-
-        self.token = response.cookies['wp_access_token']
-
-        expiration = response.cookies['wp_expiration']
-        self.expiration_date = int(expiration)//1000
-
-    # Access token provided by spotipy does not have enough permissions to start
-    # Spotify on Chromecast
-    def start_session(self):
-        """ Starts session to get access token. """
-
-        # arbitrary value and can be static
-        cookies = {"__bon": "MHwwfC01ODc4MjExMzJ8LTI0Njg4NDg3NTQ0fDF8MXwxfDE="}
-
-        if self.username is None:
-            self.username = os.getenv("SPOTIFY_USERNAME")
-
-        if self.password is None:
-            self.password = os.getenv("SPOTIFY_PASS")
-
-        if self.username is None or self.password is None:
-            raise Exception("No username or password")
-
-        session = requests.Session()
-        token = self. _get_csrf(session, cookies)
-
-        self._login(session, cookies, self.username, self.password, token)
-        self._get_access_token(session, cookies)
-
-        self.session_started = True
-
-        return self.token
 
     def launch_app(self):
         """ Launch main application """
@@ -121,7 +56,12 @@ class SpotifyController(BaseController):
             self.launch(callback_function=callback)
         else:
             self.logger.debug("Creating new token")
-            self.start_session()
+
+            data = spotify_token.start_session(self.username, self.password)
+            self.session_started = True
+            self.token = data[0]
+            self.expiration_date = data[1]
+
             self.logger.debug("Token is: %s", self.token)
             self.launch(callback_function=callback)
 
