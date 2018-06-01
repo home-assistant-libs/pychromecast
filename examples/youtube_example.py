f63485c846dcac4f0d0fe447c4dfae8d5e409591
Uri Katz <4urikatz@gmail.com>
2018-06-01 16:45:35 +0300

    Added controller example

---
+++ examples/youtube_example.py
@@ -0,0 +1,23 @@
+"""
+Example on how to use the YouTube Controller
+
+"""
+
+import pychromecast
+from pychromecast.controllers.youtube import YouTubeController
+
+
+# Change to the name of your Chromecast
+CAST_NAME = "Living Room TV"
+
+# Change to the video id of the YouTube video
+# video id is the last part of the url http://youtube.com/watch?v=video_id
+VIDEO_ID = ""
+
+
+chromecasts = pychromecast.get_chromecasts()
+cast = next(cc for cc in chromecasts if cc.device.friendly_name == CAST_NAME)
+cast.wait()
+yt = YouTubeController()
+cast.register_handler(yt)
+yt.play_video(VIDEO_ID)
