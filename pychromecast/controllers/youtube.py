124510e95ed3e74ec58f254c6437e37c13423aef
Uri Katz <4urikatz@gmail.com>
2018-06-01 23:35:14 +0300

    Better YouTube controller (#236)
    * Improved YouTube controller
    
    * imports and documentation fixes
    
    * Changes all single-quotes to double
    
    * Removed internal controller exception, fixed pylint issues
    
    * Making flake8 happy
    
    * Moved logic to an external library
    
    * change app id in conf
    
    * Added controller example

--- pychromecast/controllers/youtube.py
+++ pychromecast/controllers/youtube.py
@@ -1,59 +1,96 @@
 """
 Controller to interface with the YouTube-app.
-
 Use the media controller to play, pause etc.
 """
-from . import BaseController
+import threading
+from casttube import YouTubeSession
 
-MESSAGE_TYPE = "type"
+from . import BaseController
+from ..error import UnsupportedNamespace
+from ..config import APP_YOUTUBE
+
+YOUTUBE_NAMESPACE = "urn:x-cast:com.google.youtube.mdx"
+TYPE_GET_SCREEN_ID = "getMdxSessionStatus"
 TYPE_STATUS = "mdxSessionStatus"
 ATTR_SCREEN_ID = "screenId"
+MESSAGE_TYPE = "type"
 
 
 class YouTubeController(BaseController):
-    """ Controller to interact with Youtube namespace. """
+    """ Controller to interact with Youtube."""
 
     def __init__(self):
         super(YouTubeController, self).__init__(
-            "urn:x-cast:com.google.youtube.mdx", "233637DE")
+            YOUTUBE_NAMESPACE, APP_YOUTUBE)
+        self.status_update_event = threading.Event()
+        self._screen_id = None
+        self._session = None
 
-        self.screen_id = None
+    def start_session_if_none(self):
+        """
+        Starts a session it is not yet initialized.
+        """
+        if not (self._screen_id and self._session):
+            self.update_screen_id()
+            self._session = YouTubeSession(screen_id=self._screen_id)
+
+    def play_video(self, video_id):
+        """
+        Play video(video_id) now. This ignores the current play queue order.
+        :param video_id: YouTube video id(http://youtube.com/watch?v=video_id)
+        """
+        self.start_session_if_none()
+        self._session.play_video(video_id)
+
+    def add_to_queue(self, video_id):
+        """
+        Add video(video_id) to the end of the play queue.
+        :param video_id: YouTube video id(http://youtube.com/watch?v=video_id)
+        """
+        self.start_session_if_none()
+        self._session.add_to_queue(video_id)
+
+    def play_next(self, video_id):
+        """
+        Play video(video_id) after the currently playing video.
+        :param video_id: YouTube video id(http://youtube.com/watch?v=video_id)
+        """
+        self.start_session_if_none()
+        self._session.play_next(video_id)
+
+    def remove_video(self, video_id):
+        """
+        Remove video(videoId) from the queue.
+        :param video_id: YouTube video id(http://youtube.com/watch?v=video_id)
+        """
+        self.start_session_if_none()
+        self._session.remove_video(video_id)
+
+    def update_screen_id(self):
+        """
+        Sends a getMdxSessionStatus to get the screenId and waits for response.
+        This function is blocking
+        If connected we should always get a response
+        (send message will launch app if it is not running).
+        """
+        self.status_update_event.clear()
+        # This gets the screenId but always throws. Couldn't find a better way.
+        try:
+            self.send_message({MESSAGE_TYPE: TYPE_GET_SCREEN_ID})
+        except UnsupportedNamespace:
+            pass
+        self.status_update_event.wait()
+        self.status_update_event.clear()
 
     def receive_message(self, message, data):
         """ Called when a media message is received. """
         if data[MESSAGE_TYPE] == TYPE_STATUS:
-            self._process_status(data.get('data'))
-
+            self._process_status(data.get("data"))
             return True
 
         return False
 
-    def play_video(self, youtube_id):
-        """
-        Starts playing a video in the YouTube app.
-
-        Only works if there is no video playing.
-        """
-        def callback():
-            """Plays requested video after app launched."""
-            self.start_play(youtube_id)
-
-        self.launch(callback_function=callback)
-
-    def start_play(self, youtube_id):
-        """
-        Sends the play message to the YouTube app.
-        """
-        msg = {
-            "type": "flingVideo",
-            "data": {
-                "currentTime": 0,
-                "videoId": youtube_id
-            }
-        }
-
-        self.send_message(msg, inc_session_id=True)
-
     def _process_status(self, status):
         """ Process latest status update. """
-        self.screen_id = status.get(ATTR_SCREEN_ID)
+        self._screen_id = status.get(ATTR_SCREEN_ID)
+        self.status_update_event.set()
