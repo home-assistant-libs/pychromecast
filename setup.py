f578e55abbfd9ef00d85fcd7e3a5353c42ab42ea
Paulus Schoutsen <balloob@gmail.com>
2019-03-09 10:12:06 -0800

    3.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open('README.rst').read()
 
 setup(
     name='PyChromecast',
-    version='2.5.2',
+    version='3.0.0',
     license='MIT',
     url='https://github.com/balloob/pychromecast',
     author='Paulus Schoutsen',
