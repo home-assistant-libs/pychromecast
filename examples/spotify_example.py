dde1faf2b61e4a7f91364c39158966fda34352eb
Erik <erik@montnemery.com>
2019-03-15 22:50:07 +0100

    Update spotify example

--- examples/spotify_example.py
+++ examples/spotify_example.py
@@ -6,19 +6,30 @@ This can be done by running the following:
 pip install spotify-token
 pip install git+https://github.com/plamere/spotipy.git
 """
+import logging
+import sys
+
 import pychromecast
 from pychromecast.controllers.spotify import SpotifyController
 import spotify_token as st
 import spotipy
 
-chromecasts = pychromecast.get_chromecasts()
-cast = chromecasts[0]
-cast.start()
-
 CAST_NAME = "My Chromecast"
-device_id = None
 
-if cast.name == CAST_NAME:
+debug = '--show-debug' in sys.argv
+if debug:
+    logging.basicConfig(level=logging.DEBUG)
+
+chromecasts = pychromecast.get_chromecasts()
+cast = None
+for _cast in chromecasts:
+    if _cast.name == CAST_NAME:
+        cast = _cast
+        break
+
+if cast:
+    cast.wait()
+    device_id = None
 
     data = st.start_session("SPOTIFY_USERNAME", "SPOTIFY_PASSWORD")
     access_token = data[0]
@@ -32,7 +43,7 @@ if cast.name == CAST_NAME:
     devices_available = client.devices()
 
     for device in devices_available['devices']:
-        if device['name'] == CAST_NAME and device['type'] == 'CastVideo':
+        if device['name'] == CAST_NAME:
             device_id = device['id']
             break
 
