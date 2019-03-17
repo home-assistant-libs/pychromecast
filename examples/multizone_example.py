a4dab3fbe5d4fde99049b4d13062f674b9271483
emontnemery <erik@montnemery.com>
2019-03-17 18:34:10 +0100

    WIP - Add multizone controller (#278)
    * Add multizone controller
    
    * Lint

---
+++ examples/multizone_example.py
@@ -0,0 +1,45 @@
+"""
+Example on how to use the Multizone (Audio Group) Controller
+
+"""
+
+import logging
+import sys
+import time
+
+import pychromecast
+from pychromecast.controllers.multizone import MultizoneController
+from pychromecast.socket_client import CONNECTION_STATUS_CONNECTED
+
+# Change to the name of your Chromecast
+CAST_NAME = "Whole house"
+
+debug = '--show-debug' in sys.argv
+if debug:
+    logging.basicConfig(level=logging.DEBUG)
+
+class connlistener:
+    def __init__(self, mz):
+        self._mz=mz
+
+    def new_connection_status(self, connection_status):
+        """Handle reception of a new ConnectionStatus."""
+        if connection_status.status == 'CONNECTED':
+            self._mz.get_members()
+
+class mzlistener:
+    def multizone_member_added(self, uuid):
+        print("New member: {}".format(uuid))
+
+    def multizone_member_removed(self, uuid):
+        print("Removed member: {}".format(uuid))
+
+chromecasts = pychromecast.get_chromecasts(timeout=2)
+cast = next(cc for cc in chromecasts if cc.device.friendly_name == CAST_NAME)
+mz = MultizoneController(cast.uuid, mzlistener())
+cast.register_handler(mz)
+cast.register_connection_listener(connlistener(mz))
+cast.wait()
+while True:
+    time.sleep(1)
+
