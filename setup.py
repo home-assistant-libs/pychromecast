831b09c4fed185a7bffe0ea330b7849d5f4e36b6
Erik <erik@montnemery.com>
2019-04-27 12:39:50 +0200

    Version bump to 3.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open('README.rst').read()
 
 setup(
     name='PyChromecast',
-    version='3.2.0',
+    version='3.2.1',
     license='MIT',
     url='https://github.com/balloob/pychromecast',
     author='Paulus Schoutsen',
