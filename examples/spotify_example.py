f5a92cd816245ba53820a7ac3b63ec18acd5f8f1
Erik Montnemery <erik@montnemery.com>
2019-05-13 07:08:20 +0200

    Update spotify controller and spotify example to handle casting to audio groups (#294)
    * Update spotify example
    * Improve matching of spotify device to handle audio groups

--- examples/spotify_example.py
+++ examples/spotify_example.py
@@ -6,7 +6,10 @@ This can be done by running the following:
 pip install spotify-token
 pip install git+https://github.com/plamere/spotipy.git
 """
+import argparse
+import http.client as http_client
 import logging
+import time
 import sys
 
 import pychromecast
@@ -16,36 +19,105 @@ import spotipy
 
 CAST_NAME = "My Chromecast"
 
-debug = '--show-debug' in sys.argv
-if debug:
+parser = argparse.ArgumentParser(
+    description="Example on how to use the Spotify Controller.")
+parser.add_argument('--show-debug', help='Enable debug log',
+                    action='store_true')
+parser.add_argument('--cast',
+                    help='Name of cast device (default: "%(default)s")',
+                    default=CAST_NAME)
+parser.add_argument('--user', help='Spotify username', required=True)
+parser.add_argument('--password', help='Spotify password', required=True)
+parser.add_argument('--uri', help='Spotify uri(s) (default: "%(default)s")',
+                    default=["spotify:track:3Zwu2K0Qa5sT6teCCHPShP"],
+                    nargs='+')
+args = parser.parse_args()
+
+if args.show_debug:
     logging.basicConfig(level=logging.DEBUG)
+    # Uncomment to enable http.client debug log
+    #http_client.HTTPConnection.debuglevel = 1
 
 chromecasts = pychromecast.get_chromecasts()
 cast = None
 for _cast in chromecasts:
-    if _cast.name == CAST_NAME:
+    if _cast.name == args.cast:
         cast = _cast
         break
 
-if cast:
-    cast.wait()
-    device_id = None
+if not cast:
+    print('No chromecast with name "{}" discovered'.format(args.cast))
+    print('Discovered casts: {}'.format(chromecasts))
+    sys.exit(1)
 
-    data = st.start_session("SPOTIFY_USERNAME", "SPOTIFY_PASSWORD")
-    access_token = data[0]
-    expires = data[1] - int(time.time())
+print('cast {}'.format(cast))
 
-    client = spotipy.Spotify(auth=access_token)
+class ConnListener:
+    def __init__(self, mz):
+        self._mz=mz
 
-    sp = SpotifyController(access_token, expires)
-    cast.register_handler(sp)
-    sp.launch_app()
+    def new_connection_status(self, connection_status):
+        """Handle reception of a new ConnectionStatus."""
+        if connection_status.status == 'CONNECTED':
+            self._mz.update_members()
 
-    devices_available = client.devices()
+class MzListener:
+    def __init__(self):
+        self.got_members=False
+    
+    def multizone_member_added(self, uuid):
+        pass
 
-    for device in devices_available['devices']:
-        if device['name'] == CAST_NAME:
-            device_id = device['id']
-            break
+    def multizone_member_removed(self, uuid):
+        pass
 
-    client.start_playback(device_id=device_id, uris=["spotify:track:3Zwu2K0Qa5sT6teCCHPShP"])
+    def multizone_status_received(self):
+        self.got_members=True
+
+# Wait for connection to the chromecast
+cast.wait()
+
+spotify_device_id = None
+
+# Create a spotify token
+data = st.start_session(args.user, args.password)
+access_token = data[0]
+expires = data[1] - int(time.time())
+
+# Create a spotify client
+client = spotipy.Spotify(auth=access_token)
+if args.show_debug:
+    spotipy.trace = True
+    spotipy.trace_out = True
+
+# Launch the spotify app on the cast we want to cast to
+sp = SpotifyController(access_token, expires)
+cast.register_handler(sp)
+sp.launch_app()
+
+if not sp.is_launched and not sp.credential_error:
+    print('Failed to launch spotify controller due to timeout')
+    sys.exit(1)
+if not sp.is_launched and sp.credential_error:
+    print('Failed to launch spotify controller due to credential error')
+    sys.exit(1)
+
+# Query spotify for active devices
+devices_available = client.devices()
+
+# Match active spotify devices with the spotify controller's device id
+for device in devices_available['devices']:
+    if device['id'] == sp.device:
+        spotify_device_id = device['id']
+        break
+
+if not spotify_device_id:
+    print('No device with id "{}" known by Spotify'.format(sp.device))
+    print('Known devices: {}'.format(devices_available['devices']))
+    sys.exit(1)
+
+# Start playback
+if args.uri[0].find('track') > 0:
+    client.start_playback(device_id=spotify_device_id, uris=args.uri)
+else:
+    client.start_playback(device_id=spotify_device_id, context_uri=args.uri[0])
--- pychromecast/controllers/spotify.py
+++ pychromecast/controllers/spotify.py
@@ -2,15 +2,18 @@
 Controller to interface with Spotify.
 """
 import logging
-import time
+import threading
 
 from . import BaseController
 from ..config import APP_SPOTIFY
 from ..error import LaunchError
 
 APP_NAMESPACE = "urn:x-cast:com.spotify.chromecast.secure.v1"
-TYPE_STATUS = "setCredentials"
-TYPE_RESPONSE_STATUS = 'setCredentialsResponse'
+TYPE_GET_INFO = "getInfo"
+TYPE_GET_INFO_RESPONSE = "getInfoResponse"
+TYPE_SET_CREDENTIALS = "setCredentials"
+TYPE_SET_CREDENTIALS_ERROR = 'setCredentialsError'
+TYPE_SET_CREDENTIALS_RESPONSE = 'setCredentialsResponse'
 
 
 # pylint: disable=too-many-instance-attributes
@@ -30,13 +33,24 @@ class SpotifyController(BaseController):
         self.access_token = access_token
         self.expires = expires
         self.is_launched = False
+        self.device = None
+        self.credential_error = False
+        self.waiting = threading.Event()
     # pylint: enable=useless-super-delegation
 
     # pylint: disable=unused-argument,no-self-use
     def receive_message(self, message, data):
-        """ Currently not doing anything with received messages. """
-        if data['type'] == TYPE_RESPONSE_STATUS:
+        """ Handle the auth flow and active player selection """
+        if data['type'] == TYPE_SET_CREDENTIALS_RESPONSE:
+            self.send_message({'type': TYPE_GET_INFO, 'payload': {}})
+        if data['type'] == TYPE_SET_CREDENTIALS_ERROR:
+            self.device = None
+            self.credential_error = True
+            self.waiting.set()
+        if data['type'] == TYPE_GET_INFO_RESPONSE:
+            self.device = data['payload']['deviceID']
             self.is_launched = True
+            self.waiting.set()
         return True
 
     def launch_app(self, timeout=10):
@@ -49,16 +63,17 @@ class SpotifyController(BaseController):
 
         def callback():
             """Callback function"""
-            self.send_message({"type": TYPE_STATUS,
+            self.send_message({"type": TYPE_SET_CREDENTIALS,
                                "credentials": self.access_token,
                                "expiresIn": self.expires})
 
+        self.device = None
+        self.credential_error = False
+        self.waiting.clear()
         self.launch(callback_function=callback)
 
         # Need to wait for Spotify to be launched on Chromecast completely
-        while not self.is_launched and timeout:
-            time.sleep(1)
-            timeout -= 1
+        self.waiting.wait(timeout)
 
         if not self.is_launched:
             raise LaunchError(
