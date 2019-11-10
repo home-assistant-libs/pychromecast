7a0eb8916f12f4cae34c468a800a9b134a1afa9f
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-11-10 14:34:05 +0200

    Add support for casting with Yle Areena app.
    https://areena.yle.fi/

---
+++ examples/yleareena_example.py
@@ -0,0 +1,56 @@
+"""
+Example on how to use the Yle Areena Controller
+
+"""
+
+import re
+import pychromecast
+import argparse
+from pychromecast.controllers.yleareena import YleAreenaController
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+# Change to the name of your Chromecast
+CAST_NAME = "My Chromecast"
+
+parser = argparse.ArgumentParser(
+    description="Example on how to use the Yle Areena Controller.")
+parser.add_argument('--show-debug', help='Enable debug log',
+                    action='store_true')
+parser.add_argument('--cast',
+                    help='Name of cast device (default: "%(default)s")',
+                    default=CAST_NAME)
+parser.add_argument('--program', help='Areena Program ID',
+                    default="1-50097921")
+parser.add_argument('--audio_language', help='audio_language',
+                    default="")
+parser.add_argument('--text_language', help='text_language',
+                    default="off")
+args = parser.parse_args()
+
+if args.show_debug:
+    logging.basicConfig(level=logging.DEBUG)
+
+
+def get_entry_id(program_id):
+    from requests_html import HTMLSession
+    session = HTMLSession()
+    response = session.get('https://areena.yle.fi/{}'.format(program_id))
+    response.html.render()
+
+    entry_id = None
+    for div in response.html.find('div'):
+        if div.attrs.get('id', '').startswith('yle-kaltura-player'):
+            entry_id = re.sub(r'yle-kaltura-player\d+-\d+-', '', div.attrs['id'])
+            break
+
+    return entry_id
+
+chromecasts = pychromecast.get_chromecasts()
+cast = next(cc for cc in chromecasts if cc.device.friendly_name == args.cast)
+cast.wait()
+yt = YleAreenaController()
+cast.register_handler(yt)
+yt.play_media(entry_id=get_entry_id(args.program), audio_language=args.audio_language, text_language=args.text_language)
---
+++ pychromecast/controllers/yleareena.py
@@ -0,0 +1,86 @@
+"""
+Controller to interface with the Yle Areena app namespace.
+"""
+
+from ..config import APP_YLEAREENA
+from .media import (
+    MediaController, STREAM_TYPE_BUFFERED, TYPE_LOAD, MESSAGE_TYPE
+)
+
+
+class YleAreenaController(MediaController):
+    """ Controller to interact with Yle Areena app namespace. """
+
+    # pylint: disable=useless-super-delegation
+    def __init__(self):
+        super(YleAreenaController, self).__init__()
+
+        self.app_id = APP_YLEAREENA
+
+    # pylint: disable=too-many-arguments
+    def play_media(
+        self,
+        entry_id="",
+        audio_language="",
+        text_language="off",
+        current_time=0,
+        autoplay=True,
+        stream_type=STREAM_TYPE_BUFFERED,
+    ):
+        """
+        Play media with the entry id "entry_id".
+        This value can be found by loading a page on Areena, e.g. https://areena.yle.fi/1-50097921
+        And finding the kaltura player which has an id of yle-kaltura-player3430579305188-29-0_whwjqpry
+        In this case the entry id is 0_whwjqpry
+        """
+
+        # pylint: disable=too-many-locals
+        def app_launched_callback():
+            """Plays media after chromecast has switched to requested app."""
+
+            self._send_start_play_media(
+                entry_id,
+                audio_language,
+                text_language,
+                current_time,
+                autoplay,
+                stream_type,
+            )
+
+        receiver_ctrl = self._socket_client.receiver_controller
+        receiver_ctrl.launch_app(self.app_id, callback_function=app_launched_callback)
+
+    def _send_start_play_media(
+        self,
+        entry_id,
+        audio_language="",
+        text_language="off",
+        current_time=0,
+        autoplay=True,
+        stream_type=STREAM_TYPE_BUFFERED,
+    ):
+        # pylint: disable=too-many-locals
+        msg = {
+            "media": {
+                "streamType": stream_type,
+                "customData": {
+                    "mediaInfo": {
+                        "entryId": entry_id,
+                    },
+                    "audioLanguage": audio_language,
+                    "textLanguage": text_language,
+                },
+            },
+            MESSAGE_TYPE: TYPE_LOAD,
+            "currentTime": current_time,
+            "autoplay": autoplay,
+            "customData": {},
+            "textTrackStyle": {
+                "foregroundColor": "#FFFFFFFF",
+                "backgroundColor": "#000000FF",
+                "fontScale": 1,
+                "fontFamily": "sans-serif"
+            }
+        }
+
+        self.send_message(msg, inc_session_id=True)
