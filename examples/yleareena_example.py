ca16f9fbdc659faf56f2b99be7f0dfa1f00ec858
Eero Vilpponen <eero.vilpponen@gmail.com>
2020-05-03 07:52:40 +0300

    Better example, using yle-dl.

--- examples/yleareena_example.py
+++ examples/yleareena_example.py
@@ -3,10 +3,10 @@ Example on how to use the Yle Areena Controller
 
 """
 
-import re
 import pychromecast
 import argparse
 from pychromecast.controllers.yleareena import YleAreenaController
+from time import sleep
 
 import logging
 logger = logging.getLogger(__name__)
@@ -34,24 +34,37 @@ if args.show_debug:
     logging.basicConfig(level=logging.DEBUG)
 
 
-def get_entry_id(program_id):
-    from requests_html import HTMLSession
-    session = HTMLSession()
-    response = session.get('https://areena.yle.fi/{}'.format(program_id))
-    response.html.render()
+def get_kaltura_id(program_id):
+    """
+    Dive into the yledl internals and fetch the kaltura player id.
+    This can be used with Chromecast
+    """
+    from yledl.streamfilters import StreamFilters
+    from yledl.http import HttpClient
+    from yledl.localization import TranslationChooser
+    from yledl.extractors import extractor_factory
+    from yledl.titleformatter import TitleFormatter
 
-    entry_id = None
-    for div in response.html.find('div'):
-        if div.attrs.get('id', '').startswith('yle-kaltura-player'):
-            entry_id = re.sub(r'yle-kaltura-player\d+-\d+-', '', div.attrs['id'])
-            break
+    title_formatter = TitleFormatter()
+    language_chooser = TranslationChooser('fin')
+    httpclient = HttpClient(None)
+    stream_filters = StreamFilters()
 
-    return entry_id
+    url = 'https://areena.yle.fi/{}'.format(program_id)
+
+    extractor = extractor_factory(url, stream_filters, language_chooser, httpclient)
+    pid = extractor.program_id_from_url(url)
+
+    info = extractor.program_info_for_pid(pid, url, title_formatter, None)
+
+    return info.media_id.split('-')[-1]
 
 
 chromecasts = pychromecast.get_chromecasts()
 cast = next(cc for cc in chromecasts if cc.device.friendly_name == args.cast)
+print(cast)
 cast.wait()
 yt = YleAreenaController()
 cast.register_handler(yt)
-yt.play_areena_media(entry_id=get_entry_id(args.program), audio_language=args.audio_language, text_language=args.text_language)
+yt.play_areena_media(entry_id=get_kaltura_id(args.program), audio_language=args.audio_language, text_language=args.text_language)
+sleep(10)
