00027dc7059a512b8926e64bc45abf44c99afa2d
Erik Montnemery <erik@montnemery.com>
2020-10-16 17:18:17 +0200

    Remove Python 2 classifier

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
