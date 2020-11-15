55689d47da200612dd43e9a176a8cbbf23d4754d
Eero Vilpponen <eero.vilpponen@gmail.com>
2020-11-15 11:14:38 +0200

    Fix argument names

--- pychromecast/controllers/bubbleupnp.py
+++ pychromecast/controllers/bubbleupnp.py
@@ -15,8 +15,8 @@ class BubbleUPNPController(MediaController):
         self.app_id = APP_BUBBLEUPNP
         self.supporting_app_id = APP_BUBBLEUPNP
 
-    def quick_play(self, url, content_type='video/mp4', **kwargs):
+    def quick_play(self, media_id=None, media_type='video/mp4', **kwargs):
         """ Quick Play """
         self.play_media(
-            url, content_type, **kwargs
+            media_id, media_type, **kwargs
         )
