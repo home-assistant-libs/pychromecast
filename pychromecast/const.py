02fb5113781d3c4f5b0eea0d2d348364bebc73d5
theychx <theychx@users.noreply.github.com>
2021-01-19 10:39:26 +0100

    Add more google devices to consts (#448)

--- pychromecast/const.py
+++ pychromecast/const.py
@@ -16,6 +16,8 @@ CAST_TYPES = {
     "chromecast audio": CAST_TYPE_AUDIO,
     "google home": CAST_TYPE_AUDIO,
     "google home mini": CAST_TYPE_AUDIO,
+    "google nest mini": CAST_TYPE_AUDIO,
+    "nest audio": CAST_TYPE_AUDIO,
     "google cast group": CAST_TYPE_GROUP,
 }
 
