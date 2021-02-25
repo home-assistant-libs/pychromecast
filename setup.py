6f7b3661c72163b7a3105d59ee1bebac4105b5f9
Erik Montnemery <erik@montnemery.com>
2021-02-25 13:30:13 +0100

    Bump version to 8.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open("README.rst").read()
 
 setup(
     name="PyChromecast",
-    version="8.1.0",
+    version="8.1.1",
     license="MIT",
     url="https://github.com/balloob/pychromecast",
     author="Paulus Schoutsen",
