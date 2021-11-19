8b5cd0315801ef2a5ef105f90493f89b30cde649
Erik <erik@montnemery.com>
2021-11-19 16:54:27 +0100

    Catch zeroconf errors

--- pychromecast/models.py
+++ pychromecast/models.py
@@ -1,7 +1,15 @@
 """
 Chromecast types
 """
+import asyncio
 from collections import namedtuple
+
+import zeroconf
+
+ZEROCONF_ERRORS = (IOError, asyncio.TimeoutError)
+if hasattr(zeroconf, "EventLoopBlocked"):
+    # Added in zeroconf 0.37.0
+    ZEROCONF_ERRORS = (*ZEROCONF_ERRORS, zeroconf.EventLoopBlocked)
 
 CastInfo = namedtuple(
     "CastInfo",
