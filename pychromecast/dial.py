4b692b25b0c0e0e499cdd36fab467acc3e085144
Colin Robbins <ColinRobbins@users.noreply.github.com>
2022-01-09 17:31:08 +0000

    Log error code on exception

--- pychromecast/dial.py
+++ pychromecast/dial.py
@@ -137,8 +137,8 @@ def get_cast_type(cast_info, zconf=None, timeout=30, context=None):
                 cast_type = CAST_TYPE_AUDIO
             _LOGGER.debug("cast type: %s, manufacturer: %s", cast_type, manufacturer)
 
-        except (urllib.error.HTTPError, urllib.error.URLError, OSError, ValueError):
-            _LOGGER.warning("Failed to determine cast type")
+        except (urllib.error.HTTPError, urllib.error.URLError, OSError, ValueError) as err:
+            _LOGGER.warning("Failed to determine cast type (%s)", err)
             cast_type = CAST_TYPE_CHROMECAST
 
     return CastInfo(
