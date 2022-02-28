home-assistant-libs/pychromecast

commit 6dc378b39e657dd5d5b03e175f2fc090c79475c4

Log error code on exception (#586)

pychromecast/dial.py
@@ -137,8 +137,8 @@ def get_cast_type(cast_info, zconf=None, timeout=30, context=None):
                 cast_type = CAST_TYPE_AUDIO
             _LOGGER.debug("cast type: %s, manufacturer: %s", cast_type, manufacturer)
 
-        except (urllib.error.HTTPError, urllib.error.URLError, OSError, ValueError):
-            _LOGGER.warning("Failed to determine cast type")
+        except (urllib.error.HTTPError, urllib.error.URLError, OSError, ValueError) as err:
+            _LOGGER.warning("Failed to determine cast type (%s)", err)
             cast_type = CAST_TYPE_CHROMECAST
 
     return CastInfo(

commit e07b1b3e09de729890a40feb2683ac333928fd01

Fix black fail introduced by #586

pychromecast/dial.py
@@ -137,7 +137,12 @@ def get_cast_type(cast_info, zconf=None, timeout=30, context=None):
                 cast_type = CAST_TYPE_AUDIO
             _LOGGER.debug("cast type: %s, manufacturer: %s", cast_type, manufacturer)
 
-        except (urllib.error.HTTPError, urllib.error.URLError, OSError, ValueError) as err:
+        except (
+            urllib.error.HTTPError,
+            urllib.error.URLError,
+            OSError,
+            ValueError,
+        ) as err:
             _LOGGER.warning("Failed to determine cast type (%s)", err)
             cast_type = CAST_TYPE_CHROMECAST
 