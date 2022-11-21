ba30afcc0138c4a749a43d430f419d99a5eb7fb9
Erik Montnemery <erik@montnemery.com>
2022-11-21 10:33:05 +0100

    Bump version to 13.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open("README.rst").read()
 
 setup(
     name="PyChromecast",
-    version="12.1.4",
+    version="13.0.0",
     license="MIT",
     url="https://github.com/balloob/pychromecast",
     author="Paulus Schoutsen",
