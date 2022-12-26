f0801cee753b4cc84638a5a6a0032d3869736edc
strunker <nothinworthlivinfor@yahoo.com>
2022-12-25 20:50:45 -0500

    Implemented has as part of class constructor
    the hasattr check should take place only once as part of the class constructor, we simply check the object property on any subsequent call to run_once.

--- pychromecast/socket_client.py
+++ pychromecast/socket_client.py
@@ -229,6 +229,8 @@ class SocketClient(threading.Thread):
 
         self.receiver_controller.register_status_listener(self)
 
+        self.hasSelectPoll = hasattr(select,"poll")
+
     def initialize_connection(
         self,
     ):  # pylint:disable=too-many-statements, too-many-branches
@@ -565,9 +567,8 @@ class SocketClient(threading.Thread):
 
         # poll the socket, as well as the socketpair to allow us to be interrupted
         rlist = [self.socket, self.socketpair[0]]
-        availableCommands = dir(select)
         try:
-            if "poll" in availableCommands:
+            if self.hasSelectPoll == True:
                 # Map file descriptors to socket objects because select.select does not support fd > 1024
                 # https://stackoverflow.com/questions/14250751/how-to-increase-filedescriptors-range-in-python-select
                 fd_to_socket = {rlist_item.fileno(): rlist_item for rlist_item in rlist}
