751450a508e571ce4d2abc135c539580e3596d8a
Erik Montnemery <erik@montnemery.com>
2023-11-21 20:19:25 +0100

    Fix deprecated call to ssl.SSLContext without specifying protocol (#763)

--- pychromecast/dial.py
+++ pychromecast/dial.py
@@ -99,7 +99,7 @@ def _get_status(services, zconf, path, secure, timeout, context):
 
 def get_ssl_context():
     """Create an SSL context."""
-    context = ssl.SSLContext()
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     context.verify_mode = ssl.CERT_NONE
     return context
 
